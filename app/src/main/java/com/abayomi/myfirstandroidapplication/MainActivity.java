dfe27ef43fa9fdef0ef244ea36e527e89eaac998
Matho <swtworm@gmail.com>
2022-08-14 19:10:23 +0100

    Initial commit

---
+++ app/src/main/java/com/abayomi/myfirstandroidapplication/MainActivity.java
@@ -0,0 +1,54 @@
+package com.abayomi.myfirstandroidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int counter = 0;
+    private TextView count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        count = findViewById(R.id.counter);
+
+        Button plus = findViewById(R.id.plus);
+        Button minus = findViewById(R.id.minus);
+
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                increment();
+            }
+        });
+
+
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                decrement();
+            }
+        });
+    }
+
+    private void increment()
+    {
+        counter++;
+        String s = counter + "";
+        count.setText(s);
+    }
+
+    private void decrement()
+    {
+        counter--;
+        String s = counter + "";
+        count.setText(s);
+    }
+}
